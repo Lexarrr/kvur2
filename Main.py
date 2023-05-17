98b577edbf2d7c92ab9e66cd100eb3eee05674e5
Lexarrr <etc.21@yandex.ru>
2023-05-17 18:44:18 +0300

    import scrt

---
+++ Main.py
@@ -0,0 +1,20 @@
+from math import sqrt
+
+baddata = True;
+while baddata == True:
+    try:
+        a = int(input('Введите а: '))
+        b = int(input('Введите b: '))
+        c = int(input('Введите c: '))
+        baddata = False;
+    except ValueError:
+        print('Не удалось получить данные!')
+
+D = (b*b) - (4*a*c)
+
+if D>0:
+    d = sqrt(D)
+    x1 = ((-b)+d / (2 * a))
+    x2 = ((-b)-d / (2 * a))
+
+
